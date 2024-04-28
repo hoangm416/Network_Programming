8a42f5dbff623318abf5850f7995067a60f178f1
Vuong Hoang <hoangasphalt89@gmail.com>
2024-04-28 16:09:48 +0700

    BTVN ngay 22/04/2024

---
+++ time_server.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <time.h>
+
+void signalHanlder(int signo) {
+    pid_t pid = wait(NULL);
+    printf("Child process terminated, pid = %d\n", pid);
+}
+
+char* get_time(char* format) {
+    time_t rawtime;
+    struct tm * timeinfo;
+    char * buffer = (char *)malloc(80*sizeof(char));
+
+    time (&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer,80,format,timeinfo);
+    return buffer;
+}
+
+int main() {
+    // Tao socket cho ket noi
+    int listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listener == -1) {
+        perror("socket() failed");
+        return 1;
+    }
+
+    // Khai bao dia chi server
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(8000);
+
+    // Gan socket voi cau truc dia chi
+    if (bind(listener, (struct sockaddr *)&addr, sizeof(addr))) {
+        perror("bind() failed");
+        return 1;
+    }
+
+    // Chuyen socket sang trang thai cho ket noi
+    if (listen(listener, 5)) {
+        perror("listen() failed");
+        return 1;
+    }
+
+    signal(SIGCHLD, signalHanlder);
+
+    while (1) {
+        printf("Waiting for new client\n");
+        int client = accept(listener, NULL, NULL);
+        printf("New client accepted, client = %d\n", client);
+
+        if (fork() == 0) {
+            // Tien trinh con, xu ly yeu cau tu client
+            // Dong socket listener
+            close(listener);
+
+            char buf[256];
+            while (1) {
+                int ret = recv(client, buf, sizeof(buf), 0);
+                if (ret <= 0)
+                    break;
+
+                buf[ret] = 0;
+                printf("Received: %s", buf);
+
+                if (strncmp(buf, "GET_TIME ", 9) == 0) {
+                    char* format = buf + 9;
+                    char* time_str = get_time(format);
+                    send(client, time_str, strlen(time_str), 0);
+                    free(time_str);
+                }
+            }
+
+            // Ket thuc tien trinh con
+            exit(0);
+        }
+
+        // Dong socket client o tien trinh cha
+        close(client);
+    }
+
+    return 0;
+}
